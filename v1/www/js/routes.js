fe32ef218da3a30b436feff11cf1c994724ba571
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-03-22 10:22:49 +0100

    init

---
+++ v1/www/js/routes.js
@@ -0,0 +1,29 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('starWarsApp', {
+    url: '/page1',
+    templateUrl: 'templates/starWarsApp.html',
+    controller: 'starWarsAppCtrl'
+  })
+
+  .state('filmTitle', {
+    url: '/page2',
+    templateUrl: 'templates/filmTitle.html',
+    controller: 'filmTitleCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1')
+
+  
+
+});
