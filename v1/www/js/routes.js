897938cb87fd5bf58dd78f00b5083aafd894e967
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-03-22 11:22:13 +0100

    static app

--- v1/www/js/routes.js
+++ v1/www/js/routes.js
@@ -1,29 +1,21 @@
 angular.module('app.routes', [])
 
 .config(function($stateProvider, $urlRouterProvider) {
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+        .state('starWarsApp', {
+            url: '/page1',
+            templateUrl: 'templates/starWarsApp.html',
+            controller: 'starWarsFilmsCtrl'
+        })
+        .state('filmTitle', {
+            url: '/page2/:id',
+            templateUrl: 'templates/filmTitle.html',
+            controller: 'filmTitleCtrl'
+        })
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-  
-
-      .state('starWarsApp', {
-    url: '/page1',
-    templateUrl: 'templates/starWarsApp.html',
-    controller: 'starWarsAppCtrl'
-  })
-
-  .state('filmTitle', {
-    url: '/page2',
-    templateUrl: 'templates/filmTitle.html',
-    controller: 'filmTitleCtrl'
-  })
-
-$urlRouterProvider.otherwise('/page1')
-
-  
-
-});
+    $urlRouterProvider.otherwise('/page1')
+})
--- v1/www/js/services.js
+++ v1/www/js/services.js
@@ -1,10 +1,46 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('FilmService', ['$q', function($q) {
+    return {
+        getFilms: function() {
+            var deferred = $q.defer()
 
+            var films = [
+                {
+                    id: 1,
+                    title: "A New Hope",
+                    crawl: "ANH crawl"
+                },{
+                    id: 2,
+                    title: "The Empire Strikes Back",
+                    crawl: "ESB crawl"
+                },{
+                    id: 3,
+                    title: "Return of the Jedi",
+                    crawl: "ROTJ crawl"
+                }
+            ]
+
+            deferred.resolve(films)
+
+            return deferred.promise
+        },
+        getFilm: function(id) {
+            var deferred = $q.defer()
+
+            var film = {
+                id: id,
+                title: "Film " + id,
+                crawl: "Crawl for " + id
+            }
+
+            deferred.resolve(film)
+
+            return deferred.promise
+        }
+    }
 }])
 
-.service('BlankService', [function(){
+.service('BlankService', [function() {
 
-}]);
-
+}])
