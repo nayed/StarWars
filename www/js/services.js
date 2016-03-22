5469a67829ad191607ed6c4c1352e3081f122def
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-03-22 11:42:33 +0100

    dynamic data through swapi.co

--- www/js/services.js
+++ www/js/services.js
@@ -1,46 +1,30 @@
 angular.module('app.services', [])
 
-.factory('FilmService', ['$q', function($q) {
+.factory('FilmService', ['$http', '$q', function($http, $q) {
     return {
         getFilms: function() {
             var deferred = $q.defer()
 
-            var films = [
-                {
-                    id: 1,
-                    title: "A New Hope",
-                    crawl: "ANH crawl"
-                },{
-                    id: 2,
-                    title: "The Empire Strikes Back",
-                    crawl: "ESB crawl"
-                },{
-                    id: 3,
-                    title: "Return of the Jedi",
-                    crawl: "ROTJ crawl"
-                }
-            ]
-
-            deferred.resolve(films)
+            $http.get("http://swapi.co/api/films").then(function(res) {
+                //console.dir(res.data.results)
+                var results = res.data.results.map(function(result) {
+                    result.id = result.url
+                    return result
+                })
+                deferred.resolve(results)
+            })
 
             return deferred.promise
         },
-        getFilm: function(id) {
+        getFilm: function(url) {
             var deferred = $q.defer()
 
-            var film = {
-                id: id,
-                title: "Film " + id,
-                crawl: "Crawl for " + id
-            }
-
-            deferred.resolve(film)
+            $http.get(url).then(function(res) {
+                //console.dir(res.data)
+                deferred.resolve(res.data)
+            })
 
             return deferred.promise
         }
     }
-}])
-
-.service('BlankService', [function() {
-
 }])
